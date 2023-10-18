1e4a785b51a9ae20476a08ac9d1ce19080c06a2a
AhmedAbdelrahmaaan <146885224+AhmedAbdelrahmaaan@users.noreply.github.com>
2023-10-18 15:10:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ time_format = "%Y-%m-%d  %H:%M:%S.%f"
 Now = dt.now().replace(tzinfo=None)
 utc = dt.utcnow().replace(tzinfo=pytz.UTC).replace(tzinfo=None)
 timediff = Now - utc
-Now = dt.utcnow().replace(tzinfo=pytz.UTC)+3
+Now = time.strftime("%H:%M:%S")
 st.session_state.date = str(Now.date())
 
 
